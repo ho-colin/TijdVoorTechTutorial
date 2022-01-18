eb34e7a1e3723eca6519be6129e67c605edf22fe
Colin Meerschman <colin.meerschman@student.hogent.be>
2022-01-18 09:38:38 +0100

    Add project files.

---
+++ Les1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Les1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("TijdVoorTech");
+
+            string invoer = Console.ReadLine();
+            Console.WriteLine(invoer);
+        }
+    }
+}
